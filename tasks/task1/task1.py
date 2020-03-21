cb1ea8b235224bfea584707096cc9eb90b950a0a
Roman Isaienko <risayenko@gmail.com>
2020-03-21 20:33:56 +0100

    task1 draft

---
+++ tasks/task1/task1.py
@@ -0,0 +1,61 @@
+import math
+
+import pandas as pd
+
+
+def count_most_freq_type(types):
+    counter = {}
+    for t in types['types']:
+        if t in counter:
+            counter[t] += 1
+        else:
+            counter[t] = 1
+    return max(counter, key=counter.get)
+
+
+iris_data_test = pd.read_csv("iris_test.txt", header=None, delimiter="    	 ", engine='python')
+iris_data_training = pd.read_csv("iris_training.txt", header=None, delimiter="    	", engine='python')
+iris_data_training = iris_data_training.rename(columns={len(iris_data_training.columns)-1: "types"}, errors='raise')
+iris_data_test = iris_data_test.rename(columns={len(iris_data_test.columns)-1: "types"}, errors='raise')
+
+for c in range(len(iris_data_test.columns) - 1):
+    iris_data_training[c].astype('float')
+    iris_data_test[c].astype('float')
+    min_val = iris_data_training[c].min() if iris_data_training[c].min() < iris_data_test[c].min() \
+        else iris_data_test[c].min()
+    max_val = iris_data_training[c].max() if iris_data_training[c].max() > iris_data_test[c].max() \
+        else iris_data_test[c].max()
+    iris_data_test[c] = (iris_data_test[c] - min_val) / max_val
+    iris_data_training[c] = (iris_data_training[c] - min_val) / max_val
+
+k = 5
+k = int(input("Enter k: "))
+iris_data_training['distance'] = 0
+iris_data_test['guess'] = ""
+for index, row in iris_data_test.iterrows():
+    for ind, r in iris_data_training.iterrows():
+        dist = 0
+        # print(row, r)
+        for x in range(len(iris_data_test.columns) - 2):
+            dist += (r[x] - row[x])**2
+        dist = math.sqrt(dist)
+        r['distance'] = dist
+        # print(row)
+        iris_data_training.iloc[ind] = r
+        # break
+        # compute distance and add to col guess in training df
+    # print(iris_data_training.sort_values('distance').head(k)['distance'])
+    # print(tmp[len(iris_data_training.columns)-2])
+    row['guess'] = count_most_freq_type(iris_data_training.sort_values('distance').head(k))
+    # print(row)
+    iris_data_test.iloc[index] = row
+    # print(row)
+    # make a decision and store
+    # break
+print(iris_data_test)
+# take every row from test and calculate the difference to all cases in training set, take top k smallest differences
+# and select the most occurring one as the type
+# calculate % of correct predictions and display model accuracy
+
+# print(iris_data_training.head())
+# print(iris_data_test.head())
