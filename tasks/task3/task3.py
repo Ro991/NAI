89e61b9ca608f8d260e7304b836e71a35b736a3f
Ro991 <risayenko@gmail.com>
2020-04-15 19:46:50 +0200

    draft update

--- tasks/task3/task3.py
+++ tasks/task3/task3.py
@@ -3,6 +3,51 @@ import os
 import re
 
 
+class Perceptron:
+    weights = []
+    inputs = 0
+
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.weights = [1] * inputs
+
+    def predict(self, inputs):
+        threshold = 0.0
+        total_activation = 0.0
+        for input, weight in zip(inputs, self.weights):
+            total_activation += input * weight
+        return 1.0 if total_activation >= threshold else 0.0
+
+    def accuracy(self, matrix):
+        correct = 0.0
+        preds = []
+        for i in range(len(matrix)):
+            pred = self.predict(matrix[i][:-1], self.weights)
+            preds.append(pred)
+            if pred == matrix[i][-1]:
+                correct += 1.0
+        print("Predictions:", preds)
+        return correct / float(len(matrix))
+
+    def train(self, matrix, epochs=10, l_rate=1.0, stop_early=True):
+        for epoch in range(epochs):
+            cur_acc = self.accuracy(matrix, self.weights)
+            print("\nEpoch %d \nWeights: " % epoch, self.weights)
+            print("Accuracy: ", cur_acc)
+
+            if cur_acc == 1.0 and stop_early:
+                break
+
+            for i in range(len(matrix)):
+                prediction = self.predict(matrix[i][:-1], self.weights)
+                error = matrix[i][-1] - prediction
+                for k in range(len(self.weights)):
+                    self.weights[k] = self.weights[k] + (l_rate * error * matrix[i][k])
+
+    def __str__(self):
+        return self.weights.__str__()
+
+
 def read_texts():
     file = open(filename, encoding='utf-8')
     file_lang = filename.split("\\")[1].split('.')[0]
@@ -13,43 +58,6 @@ def read_texts():
         arr = ([x[1] for x in letter_freq])
         arr.append(file_lang)
         data.append(arr)
-
-
-def predict(inputs, weights):
-    threshold = 0.0
-    total_activation = 0.0
-    for input, weight in zip(inputs, weights):
-        total_activation += input * weight
-    return 1.0 if total_activation >= threshold else 0.0
-
-
-def accuracy(matrix, weights):
-    correct = 0.0
-    preds = []
-    for i in range(len(matrix)):
-        pred = predict(matrix[i][:-1], weights)
-        preds.append(pred)
-        if pred == matrix[i][-1]:
-            correct += 1.0
-    print("Predictions:", preds)
-    return correct / float(len(matrix))
-
-
-def train(matrix, weights, epochs=10, l_rate=1.0, stop_early=True):
-    for epoch in range(epochs):
-        cur_acc = accuracy(matrix, weights)
-        print("\nEpoch %d \nWeights: " % epoch, weights)
-        print("Accuracy: ", cur_acc)
-
-        if cur_acc == 1.0 and stop_early:
-            break
-
-        for i in range(len(matrix)):
-            prediction = predict(matrix[i][:-1], weights)
-            error = matrix[i][-1] - prediction
-            for k in range(len(weights)):
-                weights[k] = weights[k] + (l_rate * error * matrix[i][k])
-    return weights
 
 
 latin_letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z',
@@ -65,4 +73,9 @@ for key in files:
     filename = files[key]
     read_texts()
 
+perceptrons = []
+for x in range(len(langs)):
+    perceptrons.append(Perceptron(len(latin_letters)))
+
+    print(perceptrons[x])
 
